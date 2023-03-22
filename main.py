c13fa46088257f5c33f3811f86974693e05c0a55
PRODUCTION <dinodrongo@gmail.com>
2023-03-23 00:30:53 +0300

    the code is covered by docstrings

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from utils.class_Product import Product
 
+"Создаём экземпляр класса 'telephone'"
 telephone = Product("Iphone14ProMax", "159000", "5")
+
+"Добавляем экземпляр в список"
 telephone.app_prod_list(telephone)
 print()
+
+"Выводим данные без приминения скидки"
 print("До применения скидки:")
 print(telephone.get_product()+"\n")
+
+"Выводим данные после приминения скидки"
 print("После применения скидки:")
+
+"Применяем скидку"
 print(telephone.get_discount())
 print()
+
+"Добавляем экземпляр в список"
 print(f"В списке:{telephone.all}")
 
 
-
+"Повторяем вышепроделанное с новым экземпляром"
 game_console = Product("SonyPlayStation5", "52000", "5")
 game_console.app_prod_list(game_console)
 print()
@@ -20,4 +31,6 @@ print(game_console.get_product()+"\n")
 print("После применения скидки:")
 print(game_console.get_discount())
 print()
+
+"Выводим список экземпляров"
 print(f"В списке:{Product.all}")
